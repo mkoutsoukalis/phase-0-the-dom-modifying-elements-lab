26feb6d96e514ae4577a97bb5e963615111bc273
Maria Koutsoukalis <mkoutsoukalis@outlook.com>
2023-09-04 11:57:16 -0400

    Completed

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+const mainElement = document.querySelector('main#main');
+mainElement.remove();
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.innerHTML = 'Apollo is the champion';
+document.body.appendChild(newHeader);
